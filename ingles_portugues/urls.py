a98fcfa3c30ea8e1820393787b031dec6a68a2d5
“Alan” <alanbmc00@gmail.com>
2024-05-19 16:45:01 -0400

    alterações na voz

--- ingles_portugues/urls.py
+++ ingles_portugues/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('palavras/' , views.palavras, name='palavras'),
-    path('verifica_resposta/', views.verifica_resposta, name='verifica_resposta')
+    path('palavras/' , views.palavras, name='treinamento'),
+    path('verifica_resposta/', views.verifica_resposta, name='verifica_resposta'),
+    path('get_audio/<int:id_palavra>/<str:language>', views.get_audio, name='falando'),
 ]
--- ingles_portugues/views.py
+++ ingles_portugues/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Palavras
 from django.contrib import messages
-
+from deep_translator import GoogleTranslator
+import pyttsx3
+from django.http import JsonResponse
+import requests
+import os
+import pygame
+import base64
 # Create your views here.
 def home(request):
     return render (request, 'home.html')
@@ -9,7 +15,8 @@ def home(request):
 def palavras(request):
     resultado = request.session.get('resultado')
     palavras = Palavras.objects.all()[:12]
-    return render(request, 'palavras.html', {'palavras':palavras, 'resultado': resultado})
+    return render(request, 'treinamento.html', {'palavras':palavras, 'resultado': resultado})
+
 
 
 def verifica_resposta(request):
@@ -27,7 +34,38 @@ def verifica_resposta(request):
        
         messages.error(request, 'Resposta incorreta!')
 
-    return redirect('palavras')  
+    return redirect('treinamento')
 
-def ouvir_audio_portugues(request):
-    print('oi')
+
+
+
+
+def get_pronunciation(text, language):
+    """Obtém o áudio de pronúncia e o codifica em base64."""
+    google_tts_url = "https://translate.google.com/translate_tts"
+    params = {
+        "ie": "UTF-8",
+        "q": text,
+        "tl": language,
+        "client": "tw-ob",
+    }
+
+    response = requests.get(google_tts_url, params=params)
+    if response.status_code == 200:
+        audio_base64 = base64.b64encode(response.content).decode('utf-8')
+        return audio_base64
+    else:
+        return None
+
+def get_audio(request, id_palavra, language):
+    if language == 'en':
+        palavra = Palavras.objects.get(id=id_palavra).palavra_ingles.lower()
+    elif language == 'pt':
+        palavra = Palavras.objects.get(id=id_palavra).palavra_portugues.lower()
+    elif language == 'ptt':
+        palavra = Palavras.objects.get(id=id_palavra).palavra_pronuncia.lower()
+        language = 'en'
+    print(palavra,id_palavra, language)
+    audio_base64 = get_pronunciation(palavra, language)
+    return JsonResponse({'audio': audio_base64})
+
